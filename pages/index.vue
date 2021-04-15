4bdee2d4c6b7aa30aa83639ad51f59d4f09f8df7
Andres <andres_sandro@hotmail.com>
2021-04-15 19:31:04 -0300

    good login

--- pages/index.vue
+++ pages/index.vue
@@ -7,10 +7,40 @@
 </template>
 
 <script>
-
+import Cookie from "js-cookie";
+import JWTDecode from "jwt-decode";
+import cookieparser from "cookieparser";
+import { usersCollection } from '@/services/firebase.js'
+import { User } from '../model/user.js'
 
 export default {
   components: {
+    
+  },
+  async beforeCreate(){
+      const cookie = Cookie.get("access_token")
+      console.log(cookie)
+      if (cookie) {
+        const decoded = JWTDecode(cookie);
+
+        let currentUser = await usersCollection
+            .doc(decoded.user_id)
+            .get()
+            .then(function (doc) {
+              if (doc.exists) {
+                let aux = doc.data();
+                let user = new User(decoded.user_id ,aux.nick, decoded.email, aux.rol, aux.lastname, aux.firstname, aux.address, aux.phone);
+                return user;
+              } 
+            })
+            .catch(function (error) {
+              console.log("error:", error)
+            });
+        if (decoded) {
+          this.$store.commit("users/SET_USER", currentUser);
+        }
+        this.$router.push("/admin");
+      }
     
   },
   created(){
